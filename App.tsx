eb705bf3656f7bc85761c8e8fd01a7af7c6b7e68
Ajay Pradhan <pradhanajay14@gmail.com>
2023-03-07 00:13:01 +0530

    horzontal scroll

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import {View, Text, SafeAreaView, ScrollView} from 'react-native';
 import React from 'react';
 import FlatCards from './Components/FlatCards';
+import ElevatedCards from './Components/ElevatedCards';
 
 const App = () => {
   return (
     <SafeAreaView>
       <ScrollView>
-        <FlatCards/>
+        <FlatCards />
+        <ElevatedCards/>
       </ScrollView>
     </SafeAreaView>
   );
