d9ed0d664c047996683841a657239f2903e343f7
Ajay Pradhan <pradhanajay14@gmail.com>
2023-03-07 01:30:20 +0530

    deep linking and touchable opacity

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import FlatCards from './Components/FlatCards';
 import ElevatedCards from './Components/ElevatedCards';
 import FancyCard from './Components/FancyCard';
+import ActionCard from './Components/ActionCard';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <ElevatedCards />
         <FancyCard />
         <FancyCard />
+        <ActionCard/>
       </ScrollView>
     </SafeAreaView>
   );
---
+++ Components/ActionCard.tsx
@@ -0,0 +1,67 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Linking,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import React from 'react';
+
+export default function ActionCard() {
+  function openWebsite(websiteLink: string) {
+    Linking.openURL(websiteLink);
+  }
+  return (
+    <View>
+      <Text style={styles.headingText}>Blog Card</Text>
+      <View style={[styles.card, styles.elevatedCard]}>
+        <View style={styles.headingContainer}>
+          <Text style={styles.headerText}>
+            What's new in Javascript 21 - ES21
+          </Text>
+        </View>
+        <Image
+          source={{
+            uri: 'https://images.pexels.com/photos/1261427/pexels-photo-1261427.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+          }}
+          style={styles.cardImage}
+        />
+        <View style={styles.bodyContainer}>
+          <Text numberOfLines={3}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed
+            aperiam veniam ea, perferendis culpa rerum doloribus vero atque
+            tempore, consectetur impedit accusantium nobis distinctio pariatur
+            alias. Officia ratione nemo accusamus.
+          </Text>
+        </View>
+        <View style={styles.footerContainer}>
+          <TouchableOpacity
+            onPress={() => {
+              openWebsite('https://www.github.com/ajaypradhan');
+            }}>
+            <Text style={styles.socialLinks}>Read More</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 10,
+  },
+  card: {},
+  elevatedCard: {},
+  headingContainer: {},
+  headerText: {},
+  cardImage: {
+    height: 100,
+  },
+  bodyContainer: {},
+  footerContainer: {},
+  socialLinks: {},
+});
